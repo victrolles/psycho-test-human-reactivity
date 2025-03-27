f784185c93ae8066007806e4b5ab06f41d9ad95b
victrolles <95492416+victrolles@users.noreply.github.com>
2025-03-27 15:22:45 -0400

    ajout de shapes

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,15 @@
+import Shape from "../components/Shape";
+import img_bois from "../assets/bois.png";
+import img_star from "../assets/herbs.png";
 
 function Test()  {
   return (
     <div className="test">
-        <h1>Test Page</h1>
-        <p>
-            Here the test starts.
-        </p>
+        <h1>Test de réactivité</h1>
+        <Shape shape="rectangle" color="rgb(255, 0, 0)" />
+        <Shape shape="heart" texture={img_bois} />
+        <Shape shape="triangle" color="rgb(0, 255, 0)" />
+        <Shape shape="circle" texture={img_star} />
     </div>
   );
 
